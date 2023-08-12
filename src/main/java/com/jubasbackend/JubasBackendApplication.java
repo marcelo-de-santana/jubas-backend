e9939e9fe6db3d1d791af759559b73f8aaee7e11
Marcelo Nascimento de Santana <marcelo@pop-os.localdomain>
2023-08-12 15:54:15 -0300

    iniciando projeto

---
+++ src/main/java/com/jubasbackend/JubasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jubasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JubasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JubasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jubasbackend/JubasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jubasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JubasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
