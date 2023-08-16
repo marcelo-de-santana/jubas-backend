0cbc9d42b8254396a5d0d04f3348d714f093e7b7
marcelo-de-santana <marcelondesantana@outlook.com>
2023-08-16 07:32:03 -0300

    ajuste de retorno de usuário

--- src/main/java/com/jubasbackend/service/UserService.java
+++ src/main/java/com/jubasbackend/service/UserService.java
@@ -6,22 +6,28 @@ import com.jubasbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
+
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
-    public User create(User userToCreate){
-        if(userRepository.existsByEmail(userToCreate.getEmail())){
+    public User create(User userToCreate) {
+        if (userRepository.existsByEmail(userToCreate.getEmail())) {
             throw new IllegalArgumentException("Usuário já existe");
-        } else {
-            return userRepository.save(userToCreate);
         }
+        return userRepository.save(userToCreate);
     }
 
     public UserDTO findUserAccount(User user) {
         User userData = userRepository.findByEmailAndPassword(user.getEmail(), user.getPassword());
+
+        if (userData == null) {
+            return null;
+        }
+
         return new UserDTO(userData);
     }
 }
